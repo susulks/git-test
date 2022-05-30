53dbf95f8c429d81a688c042c40d11042ee777fd
susu <lks180421@163.com>
2022-05-30 14:40:54 +0800

    third commit

--- git-test/src/main/java/com/susu/git/GitTest.java
+++ git-test/src/main/java/com/susu/git/GitTest.java
@@ -2,9 +2,10 @@ package com.susu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
 
-        System.out.println("hello git");
     }
 
 }
