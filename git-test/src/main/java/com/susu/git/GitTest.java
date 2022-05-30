b01ee7cbf4a91e744cb244e7d292b026a70841a1
susu <lks180421@163.com>
2022-05-30 17:42:39 +0800

    master commit2

--- git-test/src/main/java/com/susu/git/GitTest.java
+++ git-test/src/main/java/com/susu/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
+//        冲突合并  是在合并之前对master也进行修改
 
     }
 
