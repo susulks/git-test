f17313094767230c34f3ec5cf57ab36e0e7e1d7a
susu <lks180421@163.com>
2022-05-30 14:39:59 +0800

    second commit

--- git-test/src/main/java/com/susu/git/GitTest.java
+++ git-test/src/main/java/com/susu/git/GitTest.java
@@ -3,6 +3,8 @@ package com.susu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("hello git");
     }
 
 }
